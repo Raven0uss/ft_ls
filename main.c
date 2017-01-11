d2a98441d169d0277c016fce31d1bbf06bac3d77
Sofiane BELAZOUZ <sbelazou@e3r7p17.42.fr>
2017-01-11 16:15:59 +0100

    avancement -l / -t fonctionnel, sauf pour rep specifier

--- main.c
+++ main.c
@@ -6,11 +6,98 @@
 /*   By: sbelazou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/25 15:33:33 by sbelazou          #+#    #+#             */
-/*   Updated: 2017/01/10 19:18:08 by sbelazou         ###   ########.fr       */
+/*   Updated: 2017/01/11 16:13:03 by sbelazou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+static void data_padd(char *d)
+{
+	char	str[25];
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 4;
+	d[16] = 0;
+	while (d[j])
+		str[i++] = d[j++];
+	str[i] = 0;
+	ft_putstr(str);
+}
+
+static void	aff_stat(t_ls *dc)
+{
+	ft_putnbr(dc->file->s.st_mode);
+	ft_putstr(" ");
+	ft_putnbr(dc->file->s.st_nlink);
+	ft_putstr(" ");
+	ft_putnbr(dc->file->s.st_uid);
+	ft_putstr(" ");
+	ft_putnbr(dc->file->s.st_gid);
+	ft_putstr(" ");
+	ft_putnbr(dc->file->s.st_size);
+	ft_putstr(" ");
+	data_padd(ctime(&dc->file->s.st_mtime));
+	ft_putstr(" ");
+}
+
+static char **ft_revtime(t_ls *dc, int size)
+{
+	int		i;
+	char	*tmp;
+	int		timod;
+
+	i = 0;
+	timod = 0;
+	dc->file->tab = ft_rev_tab(dc->file->tab, size);
+	stat(dc->file->tab[i], &(dc->file->s));
+	while (i < size)
+	{
+		timod = dc->file->s.st_mtime;
+		stat(dc->file->tab[i + 1], &(dc->file->s));
+		if (timod > dc->file->s.st_mtime)
+		{
+			tmp = dc->file->tab[i];
+			dc->file->tab[i] = dc->file->tab[i + 1];
+			dc->file->tab[i + 1] = tmp;
+			i = 0;
+			stat(dc->file->tab[i], &(dc->file->s));
+		}
+		else
+			i++;
+	}
+	return (dc->file->tab);
+}
+
+static char **ft_sortime(t_ls *dc, int size)
+{
+	int		i;
+	char	*tmp;
+	int	timod;
+
+	i = 0;
+	timod = 0;
+	dc->file->tab = ft_sort_tab(dc->file->tab, size);
+	stat(dc->file->tab[i], &(dc->file->s));
+	while (i < size)
+	{
+		timod = dc->file->s.st_mtime;
+		stat(dc->file->tab[i + 1], &(dc->file->s));
+		if (timod < dc->file->s.st_mtime)
+		{
+			tmp = dc->file->tab[i];
+			dc->file->tab[i] = dc->file->tab[i + 1];
+			dc->file->tab[i + 1] = tmp;
+			i = 0;
+			stat(dc->file->tab[i], &(dc->file->s));
+		}
+		else
+			i++;
+	}
+	return (dc->file->tab);
+}
 
 void	ft_ls(t_ls *dc)
 {
@@ -29,10 +116,35 @@ void	ft_ls(t_ls *dc)
 	while ((dc->file->ent = readdir(dc->dir)))
 		if (dc->file->ent->d_name[0] != '.' || ft_strchr(dc->l_args, 'a'))
 			dc->file->tab[i++] = ft_strdup(dc->file->ent->d_name);
-	if (ft_strchr(dc->l_args, 'r'))
-		ft_aff_tab(ft_rev_tab(dc->file->tab, i - 1), sep);
+	if (ft_strchr(dc->l_args, 't'))
+	{
+		if (ft_strchr(dc->l_args, 'r'))
+			dc->file->tab = ft_revtime(dc, i - 1);
+		else
+			dc->file->tab = ft_sortime(dc, i - 1);
+	}
 	else
-		ft_aff_tab(ft_sort_tab(dc->file->tab, i - 1), sep);
+	{
+		if (ft_strchr(dc->l_args, 'r'))
+			dc->file->tab = ft_rev_tab(dc->file->tab, i - 1);
+		else
+			dc->file->tab = ft_sort_tab(dc->file->tab, i - 1);
+	}
+	i = 0;
+	if (ft_strchr(dc->l_args, 'l'))
+	{
+		ft_putendl("total ?");
+		while (dc->file->tab[i])
+		{
+			stat(dc->file->tab[i], &(dc->file->s));
+			aff_stat(dc);
+			ft_putendl(dc->file->tab[i++]);
+		}
+	}
+	else
+	{
+		ft_aff_tab(dc->file->tab, sep);
+	}
 }
 
 static char	*crt_bname(char *name)
