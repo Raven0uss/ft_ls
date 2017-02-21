c82254224e34dc6307588b1331346c560264f665
Sofiane BELAZOUZ <sbelazou@e1r1p14.42.fr>
2017-02-21 04:57:13 +0100

    Push with Makefile

--- new/srcs/recurs.c
+++ new/srcs/recurs.c
@@ -6,7 +6,7 @@
 /*   By: sbelazou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 18:36:43 by sbelazou          #+#    #+#             */
-/*   Updated: 2017/02/21 03:04:26 by sbelazou         ###   ########.fr       */
+/*   Updated: 2017/02/21 04:53:21 by sbelazou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 static char	**encrust_rec(char **rec, t_data *ls, int pos)
 {
-	int		size;
-	int		i;
-	int		max;
-	int		count;
+	unsigned int		size;
+	unsigned int		i;
+	unsigned int		max;
+	unsigned int		count;
 
 	size = ft_sizetab(rec) - 1;
 	max = ft_sizetab(ls->reps);
@@ -37,7 +37,6 @@ static char	**encrust_rec(char **rec, t_data *ls, int pos)
 	free(rec);
 	return (ls->reps);
 }
-
 
 static char	**add_repository(char *to_add, char **rec, char *dir, t_data *ls)
 {
@@ -72,6 +71,35 @@ static char	**add_to_reps(char **rec, t_data *ls, int pos)
 	return (ls->reps);
 }
 
+static char	**loop_optreps(t_data *ls, char **tab, char **rec, char *repo)
+{
+	int		i;
+
+	i = 0;
+	if (!(rec = malloc(sizeof(char *) * 1024)))
+		return (NULL);
+	rec[0] = NULL;
+	if (!(ls->dir = opendir(repo)))
+		perror(repo);
+	else
+	{
+		while ((ls->ent = readdir(ls->dir)))
+			if (ls->ent->d_name[0] != '.' || ft_strchr(ls->args, 'a'))
+			{
+				tab[i] = ft_strdup(ls->ent->d_name);
+				stat(path(repo, tab[i]), &(ls->s));
+				if (ft_strchr(ls->args, 'R') && S_ISDIR(ls->s.st_mode)
+					&& (ft_strcmp(".", tab[i]) && ft_strcmp("..", tab[i])))
+					rec = add_repository(tab[i], rec, repo, ls);
+				i++;
+			}
+		tab[i] = 0;
+		aff_ls(ls, tab, i, repo);
+		closedir(ls->dir);
+	}
+	return (rec);
+}
+
 void		ft_optreps(t_data *ls, char **tab)
 {
 	int		i;
@@ -86,30 +114,9 @@ void		ft_optreps(t_data *ls, char **tab)
 	}
 	while (ls->reps[j])
 	{
-		i = 0;
-		if (!(rec = malloc(sizeof(char *) * 1024)))
+		if (!(tab = malloc(sizeof(char *) * 10240)))// Allocation non precise
 			return ;
-		rec[0] = NULL;
-		if (!(tab = malloc(sizeof(char *) * 1024))) // Allocation non precise
-			return ;
-		if (!(ls->dir = opendir(ls->reps[j])))
-			perror(ls->reps[j]);
-		else
-		{
-			while ((ls->ent = readdir(ls->dir)))
-				if (ls->ent->d_name[0] != '.' || ft_strchr(ls->args, 'a'))
-				{
-					tab[i] = ft_strdup(ls->ent->d_name);
-					stat(path(ls->reps[j], tab[i]), &(ls->s));
-					if (ft_strchr(ls->args, 'R') && S_ISDIR(ls->s.st_mode)
-						&& (ft_strcmp(".", tab[i]) && ft_strcmp("..", tab[i])))
-						rec = add_repository(tab[i], rec, ls->reps[j], ls);
-					i++;
-				}
-			tab[i] = 0;
-			aff_ls(ls, tab, i, ls->reps[j]);
-			closedir(ls->dir);
-		}
+		rec = loop_optreps(ls, tab, rec, ls->reps[j]);
 		free(tab);
 		if (rec[0] != NULL)
 			ls->reps = add_to_reps(rec, ls, j + 1);
