a3f8a07ecefe352c0cea30b2558a3872b4e4f119
Sofiane BELAZOUZ <sbelazou@e2r2p5.42.fr>
2017-03-17 22:08:17 +0100

    Push with Makefile

--- srcs/aff_lsl.c
+++ srcs/aff_lsl.c
@@ -6,7 +6,7 @@
 /*   By: sbelazou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/13 17:30:16 by sbelazou          #+#    #+#             */
-/*   Updated: 2017/03/17 18:44:08 by sbelazou         ###   ########.fr       */
+/*   Updated: 2017/03/17 22:06:22 by sbelazou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,10 @@ static void					affgiduid(uid_t uid, gid_t gid, t_data *ls)
 
 	pwd = getpwuid(uid);
 	grp = getgrgid(gid);
-	write_space(ls->ttx->usr, 0, pwd->pw_name);
 	ft_putstr(pwd->pw_name);
-	write_space(ls->ttx->grp, 0, grp->gr_name);
+	write_space(ls->ttx->usr, 0, pwd->pw_name);
 	ft_putstr(grp->gr_name);
+	write_space(ls->ttx->grp, 0, grp->gr_name);
 }
 
 static void			aff_perm(mode_t m)
@@ -97,6 +97,7 @@ void				aff_stat(t_data *ls)
 	aff_perm(ls->s.st_mode);
 	write_space(ls->ttx->lnk, ls->s.st_nlink, NULL);
 	ft_putnbr(ls->s.st_nlink);
+	ft_putchar(' ');
 	affgiduid(ls->s.st_uid, ls->s.st_gid, ls);
 	write_space(ls->ttx->sze, ls->s.st_size, NULL);
 	ft_putnbr(ls->s.st_size);
